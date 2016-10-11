145c5ec867d6c14171897c1d2b9008c635cea498
imsunhao <sunhao__159@163.com>
2016-10-12 00:38:08 +0800

    start frameing

---
+++ config.rb
@@ -0,0 +1,15 @@
+require 'bootstrap-sass'
+require 'compass/import-once/activate'
+require 'compass'
+
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+output_style =  :compact
+# relative_assets = true
+line_comments = false
+cache = false
+sourcemap = true
